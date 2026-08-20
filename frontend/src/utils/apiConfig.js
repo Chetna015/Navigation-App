db14814f7d9d136f00f2043fb824fba9b4b34651
Suryansh <guptasuryansh67@gmail.com>
2026-08-20 14:11:48 +0530

    feat: serve frontend over HTTPS via basic-ssl and configure dev proxies to route API traffic securely on local networks

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,9 +1,6 @@
 export const getApiBaseUrl = () => {
   if (typeof window !== 'undefined') {
-    const hostname = window.location.hostname;
-    if (hostname && hostname !== 'localhost' && hostname !== '127.0.0.1') {
-      return `http://${hostname}:5000`;
-    }
+    return window.location.origin;
   }
   return 'http://localhost:5000';
 };
